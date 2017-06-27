732502e86832bdbb69bdd34b0b8f695f08d08aa7
John Uberbacher <johnuberbacher@gmail.com>
2017-06-27 17:25:17 -0400

    Counts Down
    Script was counting 1 to 99 instead of 99 to 1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-for i in range(1,100):
+for i in range(100,0,-1):
 	if i == 1:
 		print("\n" + str(i) + "  bottle of beer on the wall, " + str(i) + " bottle of beer. Take one down and pass it around, no more bottles of beer on the wall.")
 	else:
